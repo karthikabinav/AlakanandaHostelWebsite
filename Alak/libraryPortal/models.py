319df5865bfff14cc91db12170ac44fe351355a4
karthikabinav <karthikabinavs@gmail.com>
2012-05-07 23:20:46 +0530

    Added the basic models for the library portal

---
+++ Alak/libraryPortal/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+from Alak.home.models import UserProfile
+
+class Book(models.model):
+    name = models.CharField(max_length=400, required=True)
+    author = models.CharField(max_length=400, required=False)
+    isVisible = models.BooleanField()
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Admin:
+        pass
+    
+class BookOrder(models.Model):
+    user = models.ForeignKey(UserProfile)
+    book = models.ForeignKey(Book)
+    dateBorrowed = models.DateTimeField()
+    dueDate = models.DateTimeField()
+    
+    class Admin:
+        pass
+
+
